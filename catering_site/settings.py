acb403751cc87842d7f3a8cd1c3577254bd1dcb4
Timi Fayomi <otimiade@gmail.com>
2026-07-02 11:41:09 +0100

    Fix settings: add ROOT_URLCONF, remove widget_tweaks

--- catering_site/settings.py
+++ catering_site/settings.py
@@ -18,7 +18,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_widget_tweaks',
     # add your app names here, e.g. 'catering'
 ]
 
