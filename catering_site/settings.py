b78fedef884a12055c26cd4881e1cfdd42660e1f
Timi Fayomi <otimiade@gmail.com>
2026-07-02 01:39:35 +0100

    add DATABASES config for Render

--- catering_site/settings.py
+++ catering_site/settings.py
@@ -9,3 +9,13 @@ except ImportError:
 load_dotenv()
 
 FERNET_KEY = os.getenv('FERNET_KEY')
+
+import dj_database_url
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
