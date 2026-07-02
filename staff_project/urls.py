9ec1a1e312e10c68a2f38ba4aa71f1f816f5f109
Timi Fayomi <otimiade@gmail.com>
2026-07-02 18:29:25 +0100

    debug health view

--- staff_project/urls.py
+++ staff_project/urls.py
@@ -7,6 +7,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 def health_check(request):
+    print("HEALTH VIEW HIT")
     return HttpResponse("ok")
 
 urlpatterns = [
