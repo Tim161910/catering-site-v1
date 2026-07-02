d843479471d317612eda3149dc3ea9482ff4780a
Timi Fayomi <otimiade@gmail.com>
2026-07-02 15:55:39 +0100

    Auto create superuser for Render

--- wsgi.py
+++ wsgi.py
@@ -14,3 +14,13 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'staff_project.settings')
 
 application = get_wsgi_application()
+
+"""
+WSGI config for staff_project project.
+"""
+# Auto-create superuser on first deploy - remove after first login!
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(is_superuser=True).exists():
+    User.objects.create_superuser('admin', 'admin@example.com', 'Admin1234!')
+    print("Superuser created: admin / Admin1234!")
