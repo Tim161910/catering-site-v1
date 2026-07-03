5271577e90be4008db34d90b7d0a176449ed30ef
Timi Fayomi <otimiade@gmail.com>
2026-07-03 06:47:00 +0100

    Safe superuser creation in build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash 
 python manage.py migrate 
-"from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('bamboo3', 'admin@test.com', 'TempPass123')" | python manage.py shell 
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='bamboo3').exists() or User.objects.create_superuser('bamboo3', 'admin@test.com', 'TempPass123')" | python manage.py shell
