3d1d0a909120273f28f96203b40531ece9f4d1bd
Timi Fayomi <otimiade@gmail.com>
2026-07-03 07:31:38 +0100

    Fix: Move superuser creation to apps.py ready() to avoid AppRegistryNotReady

--- staff/apps.py
+++ staff/apps.py
@@ -1,5 +1,14 @@
 from django.apps import AppConfig
-
+import os
 
 class StaffConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'staff'
+
+    def ready(self):
+        if os.environ.get('RENDER'):
+            from django.contrib.auth import get_user_model
+            User = get_user_model()
+            if not User.objects.filter(username='bamboo3').exists():
+                User.objects.create_superuser('bamboo3', 'admin@test.com', 'TempPass123')
+                print("Superuser bamboo3 created on startup")
