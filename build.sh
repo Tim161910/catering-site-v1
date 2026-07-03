67e1c6749f3e80c511e518b5660e3e7257eb8a65
Timi Fayomi <otimiade@gmail.com>
2026-07-03 07:05:18 +0100

    Update build.sh to print superuser status

--- build.sh
+++ build.sh
@@ -1,3 +1,11 @@
-#!/usr/bin/env bash 
-python manage.py migrate 
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='bamboo3').exists() or User.objects.create_superuser('bamboo3', 'admin@test.com', 'TempPass123')" | python manage.py shell
+#!/usr/bin/env bash
+python manage.py migrate
+python manage.py shell << EOF
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if User.objects.filter(username='bamboo3').exists():
+    print("Superuser bamboo3 already exists")
+else:
+    User.objects.create_superuser('bamboo3', 'admin@test.com', 'TempPass123')
+    print("Superuser bamboo3 created successfully")
+EOF
