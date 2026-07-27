92d045b3b7bb41fc8d5b07571d438737ee202561
Timi Fayomi <otimiade@gmail.com>
2026-07-27 07:10:09 +0100

    add seed loader url

--- staff/urls.py
+++ staff/urls.py
@@ -55,4 +55,5 @@ urlpatterns = [
     # path('notifications/mark-all-read/', views.mark_all_notifications_read, name='mark_all_notifications_read'),
     # path('assignment/<int:pk>/accept/', views.accept_assignment, name='accept_assignment'),
     # path('assignment/<int:pk>/decline/', views.decline_assignment, name='decline_assignment'),
+    path('load-seed/', views.load_seed_data, name='load_seed'),
 ]
