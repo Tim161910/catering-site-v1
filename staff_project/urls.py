74ad55e665788d6c17239820b237a2f348cf361c
Timi Fayomi <otimiade@gmail.com>
2026-08-01 11:55:51 +0100

    fix: add staff_admin url to staff_project

--- staff_project/urls.py
+++ staff_project/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.http import HttpResponse
 from django.urls import path, include
-from staff.admin import staff_admin_site
-from django.views.generic import RedirectView
+from django.contrib.auth.views import LogoutView
+from django.views.generic import TemplateView
+from staff.admin import staff_admin_site # <-- THIS IS YOUR DEMO ADMIN
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -12,8 +13,9 @@ def health_check(request):
 
 urlpatterns = [
     path('health/', health_check),
-    path('admin/', staff_admin_site.urls),
-    path('staff/', include('staff.urls')), 
-    path('', RedirectView.as_view(url='staff/'), name='home'),  
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('admin/', admin.site.urls), # your full admin - keep this for you
+    path('staff_admin/', staff_admin_site.urls), # <-- ADD THIS for demo admin
+    path('staff/', include('staff.urls', namespace='staff')),
+    path('logout/', LogoutView.as_view(next_page='admin:login'), name='logout'),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
