ce58724b8fb9520b3cc326d260cd7f8c30c8f7f0
Timi Fayomi <otimiade@gmail.com>
2026-08-02 11:51:47 +0100

    Fix logout and home to use staff login

--- staff_project/urls.py
+++ staff_project/urls.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from django.http import HttpResponse
 from django.urls import path, include
 from django.contrib.auth.views import LogoutView
-from django.views.generic import TemplateView
-from staff.admin import staff_admin_site # <-- THIS IS YOUR DEMO ADMIN
+from django.shortcuts import redirect  # ADD THIS
+from staff.admin import staff_admin_site
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -13,9 +13,9 @@ def health_check(request):
 
 urlpatterns = [
     path('health/', health_check),
-    path('admin/', admin.site.urls), # your full admin - keep this for you
-    path('staff_admin/', staff_admin_site.urls), # <-- ADD THIS for demo admin
+    path('admin/', admin.site.urls),
+    path('staff_admin/', staff_admin_site.urls),
     path('staff/', include('staff.urls', namespace='staff')),
-    path('logout/', LogoutView.as_view(next_page='admin:login'), name='logout'),
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('logout/', LogoutView.as_view(next_page='staff:staff_login'), name='logout'), # FIXED
+    path('', lambda request: redirect('staff:staff_login'), name='home'), # FIXED
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
