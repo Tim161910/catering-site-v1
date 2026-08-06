a31b674d3e7dd846104a136472a35423bf47f2a2
Timi Fayomi <otimiade@gmail.com>
2026-08-06 13:17:03 +0100

    Fix: Remove duplicate staff app in INSTALLED_APPS

--- staff_project/settings.py
+++ staff_project/settings.py
@@ -31,14 +31,13 @@ else:
     }
 
 INSTALLED_APPS = [
-    'staff',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'staff.apps.StaffConfig',
+    'staff.apps.StaffConfig',   
     'widget_tweaks',
 ]
 
